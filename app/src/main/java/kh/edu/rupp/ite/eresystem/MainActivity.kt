10965830bb401cbba69cf1299c79ee1ef7be08e5
sophavinly <ly.sophavin1122004@gmail.com>
2024-09-27 09:38:39 +0700

    add first Screen and Home Screen

---
+++ app/src/main/java/kh/edu/rupp/ite/eresystem/MainActivity.kt
@@ -0,0 +1,26 @@
+package kh.edu.rupp.ite.eresystem
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Hide the ActionBar
+        supportActionBar?.hide()
+
+        // Use Handler with the main Looper to delay the transition to HomeActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Create an Intent to start HomeActivity
+            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+            // Optionally, call finish() if you don't want to allow the user to return to MainActivity
+            finish()
+        }, 3000) // 3 second delay
+    }
+}
